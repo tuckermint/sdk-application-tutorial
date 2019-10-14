4293a01a86c574e4873afbfd84edaad2d526808b
Tuckermint <info@tuckermint.com>
2019-10-14 17:44:16 -0400

    fix bank.go

--- x/minisupply/internal/keeper/bank.go
+++ x/minisupply/internal/keeper/bank.go
@@ -9,39 +9,50 @@ import (
 
 // SendCoinsFromModuleToAccount transfers coins from a ModuleAccount to an AccAddress
 func (k Keeper) SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string,
-	panic(fmt.Sprintf("Minisupply can't use bank methods"))
+	recipientAddr sdk.AccAddress, amt sdk.Coins) sdk.Error {
+
+	panic(fmt.Sprintf("Minisupply can't use bank methods"))	
 }
 
 // SendCoinsFromModuleToModule transfers coins from a ModuleAccount to another
 func (k Keeper) SendCoinsFromModuleToModule(ctx sdk.Context, senderModule, recipientModule string, amt sdk.Coins) sdk.Error {
+
 	panic(fmt.Sprintf("Minisupply can't use bank methods"))
 }
 
 // SendCoinsFromAccountToModule transfers coins from an AccAddress to a ModuleAccount
 func (k Keeper) SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress,
+	recipientModule string, amt sdk.Coins) sdk.Error {
+
 	panic(fmt.Sprintf("Minisupply can't use bank methods"))
 }
 
 // DelegateCoinsFromAccountToModule delegates coins and transfers
 // them from a delegator account to a module account
 func (k Keeper) DelegateCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress,
+	recipientModule string, amt sdk.Coins) sdk.Error {
+
 	panic(fmt.Sprintf("Minisupply can't use bank methods"))
 }
 
 // UndelegateCoinsFromModuleToAccount undelegates the unbonding coins and transfers
 // them from a module account to the delegator account
 func (k Keeper) UndelegateCoinsFromModuleToAccount(ctx sdk.Context, senderModule string,
+	recipientAddr sdk.AccAddress, amt sdk.Coins) sdk.Error {
+
 	panic(fmt.Sprintf("Minisupply can't use bank methods"))
 }
 
 // MintCoins creates new coins from thin air and adds it to the module account.
 // Panics if the name maps to a non-minter module account or if the amount is invalid.
 func (k Keeper) MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) sdk.Error {
+
 	panic(fmt.Sprintf("Minisupply can't use bank methods"))
 }
 
 // BurnCoins burns coins deletes coins from the balance of the module account.
 // Panics if the name maps to a non-burner module account or if the amount is invalid.
 func (k Keeper) BurnCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) sdk.Error {
+
 	panic(fmt.Sprintf("Minisupply can't use bank methods"))
 }
