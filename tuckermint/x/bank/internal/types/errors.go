79aab25a166e8fd7c129f6431f91befe73fbf8cc
Tuckermint <info@tuckermint.com>
2020-02-05 01:48:22 -0500

    don't crash when bank is loaded twice

--- tuckermint/x/bank/internal/types/errors.go
+++ tuckermint/x/bank/internal/types/errors.go
@@ -4,10 +4,18 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
+func getShiftedIndex(index uint32) uint32{
+    var e = sdkerrors.ABCIError(ModuleName, index, "")
+    if e.Error() == "unknown" {
+        return index
+    }
+    return index + 4;
+}
+
 // x/bank module sentinel errors
 var (
-	ErrNoInputs            = sdkerrors.Register(ModuleName, 1, "no inputs to send transaction")
-	ErrNoOutputs           = sdkerrors.Register(ModuleName, 2, "no outputs to send transaction")
-	ErrInputOutputMismatch = sdkerrors.Register(ModuleName, 3, "sum inputs != sum outputs")
-	ErrSendDisabled        = sdkerrors.Register(ModuleName, 4, "send transactions are disabled")
+	ErrNoInputs            = sdkerrors.Register(ModuleName, getShiftedIndex(1), "no inputs to send transaction")
+	ErrNoOutputs           = sdkerrors.Register(ModuleName, getShiftedIndex(2), "no outputs to send transaction")
+	ErrInputOutputMismatch = sdkerrors.Register(ModuleName, getShiftedIndex(3), "sum inputs != sum outputs")
+	ErrSendDisabled        = sdkerrors.Register(ModuleName, getShiftedIndex(4), "send transactions are disabled")
 )
